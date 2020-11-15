ea8ea9b1f1851859e761b8b7311de0caac9a5ea5
pavel-lebedev96 <73466790+pavel-lebedev96@users.noreply.github.com>
2020-11-15 20:53:09 +0700

    Add files via upload

--- Source.c
+++ Source.c
@@ -1,46 +1,115 @@
 #define _CRT_SECURE_NO_WARNINGS
-#include <locale.h>
 #include <stdio.h>
 #include <stdbool.h>
+#include <string.h>
 #define BUFSIZE 1024
-extern bool add(const char* a_str, const char* b_str, char* res_str);
-extern bool subtract(const char* a_str, const char* b_str, char* res_str);
-extern bool multiply(const char* a_str, const char* b_str, char* res_str);
-extern bool divide(const char* a_str, const char* b_str, char* res_str);
+#define TABLE_OP_SIZE 4
+
+//информация об операции
+struct OperationInfo
+{
+	//handle для разделяемой библиотеки
+	void* handle;
+	//указатель на функцию
+	bool (*funcp)(const char* a_str, const char* b_str, char* res_str);
+	//название операции
+	char name[20];
+	//номер действия в пункте меню
+	char key;
+};
+
+//таблица операций
+struct TableOperations
+{
+	//таблица для операций
+	OperationInfo table[TABLE_OP_SIZE];
+	//текущий размер таблицы
+	size_t size = 0;
+};
+
+//добавление элемента op_info в таблицу operations
+bool addItemInTable(OperationInfo* op_info, TableOperations* operations)
+{
+	if (operations->size == TABLE_OP_SIZE - 1)
+		return false;
+	operations->table[size + 1].funcp = op_info->funcp;
+	strcpy(p_table->table[size + 1].name, op_info->name);
+	operations->table[size + 1].key = size - '0' + 1;
+	operations->size++;
+	return true;
+}
+
+//добавление операции с именем op_name в таблицу operations
+bool addOperation(const char* op_name, TableOperations* operations)
+{
+	OperationInfo op_info;
+	strcpy(op_info.name, op_name);
+
+	/*имя библиотеки в текущей директории в формате
+	"/имя библиотеки.so"*/
+	char libname[40] = "/";
+	strcat(libname, opname);
+	strcat(libname, ".so");
+	
+	//открытие библиотеки
+	op_info.handle = dlopen(libname, RTLD_LAZY);
+	if (!op_info.handle)
+		return false;
+	
+	//поиск функции
+	op_info.funcp = (bool (*)
+		(const char*, const char*, char*)) dlsym(op_info.handle, op_name);
+	if (!op_info.funcp)
+	{
+		dlclose(op_info.handle);
+		return false;
+	}
+	
+	return addItemInTable(&op_info, operations);
+}
+
+//загрузка операций
+void load(TableOperations* operations)
+{
+	char names[TABLE_OP_SIZE][20] = { "add", "substract", "multiply", "divide" };
+	for (int i = 0; i < TABLE_OP_SIZE; i++)
+		addOperation(names[i], operations);
+}
+
 int main(int argc, char* argv[])
 {
 	char mode, temp;
 	char a_str[BUFSIZE], b_str[BUFSIZE], res_str[BUFSIZE];
 	bool (*func)(const char* a_str, const char* b_str, char* res_str) = NULL;
-	setlocale(LC_CTYPE, "Russian");
+	bool flag;
+	TableOperations operations;
+	load(operations);
+	if (operations.size == 0)
+	{
+		printf("There is no operations to show\n");
+		getchar();
+		return 0;
+	}
 	do
 	{
 		mode = 'y';
-		while (mode != '1' && mode != '2' && mode != '3' && mode != '4' && mode != '5')
+		flag = false;
+		while (!flag)
 		{
 			system("clear");
-			printf("%s%s", "Нажмите следующие клавиши, для соответсвующих действий:",
-				"\n1 - сложение,\n2 - вычитание,\n3 - умножение,\n4 - деление,\n5 - выход\n");
+			for (int i = 0; i < operations.size; i++)
+				printf("%c - %s\n", operations.table[i].key, operations.table[i].name);
+			printf("q - exit\n");
 			while ((temp = getchar()) != EOF)
 				mode = temp;
-		}		
-		switch (mode)
-		{
-		case '1':
-			func = add;
-			break;
-		case '2':
-			func = subtract;
-			break;
-		case '3':
-			func = multiply;
-			break;
-		case '4':
-			func = divide;
-			break;
-		case '5':
-			mode = 'n';
-			goto skip;
+			if (mode == 'q')
+				goto skip;
+			for (int i = 0; i < operations.size; i++)
+				if (mode == operations.table[i].key)
+				{
+					func = operations.table[i].funcp;
+					flag = true;
+				}
 		}
 		system("clear");
 		printf("а:\n");
@@ -49,13 +118,13 @@ int main(int argc, char* argv[])
 		printf("b:\n");
 		scanf("%s", b_str);
 		if (func(a_str, b_str, res_str))
-			printf("Результат операции: %s\n", res_str);
+			printf("Result: %s\n", res_str);
 		else
-			printf("Ошибка!\n");
+			printf("Error!\n");
 
 		while (getchar() != EOF) {};
 		skip:;
 		
-	} while (mode != 'n');
+	} while (mode != 'q');
 	return 0;
 }
--- substract.h
+++ substract.h
@@ -1,4 +1,3 @@
-#include "utility.h"
 #ifndef SUBSTRACT_H
 #define SUBSTRACT_H
 //вычитание
@@ -6,4 +5,9 @@
 //операнды в виде строк
 //возвращает true в случае успеха, результат в виде строки в res
 bool subtract(const char* a_str, const char* b_str, char* res_str);
+
+//вспомогательные функции (utility.h)
+extern bool str_to_double(const char* str, double* num);
+extern void double_to_str(double num, char* str);
+extern bool check_fe_exceptions(double res);
 #endif
